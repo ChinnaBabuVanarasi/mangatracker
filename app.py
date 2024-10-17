c041dba1baf98668e25344a4de129bbb4bc622bf
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-10-17 14:45:33 +0530

    updated code

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import requests
 app = FastAPI()
 
 async def get_api_records(api_url):
-    url = f"https://mangatracker-502816012135.us-central1.run.app/api/{api_url}"
+    url = f"https://mangatracker-502816012135.us-central1.run.app/{api_url}"
+    print(url)
     response = requests.get(url=url)
     if response.status_code == 200:
         data = response.json()  # Parse JSON response
@@ -14,11 +15,17 @@ async def get_api_records(api_url):
     else:
         return {'error': 'Failed to fetch data'}, response.status_code
 
+@app.get('/', status_code=status.HTTP_200_OK)
+async def homepage():
+    try:
+        return {"Hi Hello!!!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
 @app.get('/manga', status_code=status.HTTP_200_OK)
 async def get_all_links():
     try:
-        records = get_api_records('links')
+        records = await get_api_records('api/links')
         return records
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
